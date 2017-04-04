1fd18d1cdc732b72b829c3e64f2f405ee93e31e5
Caleb <calebccff@gmail.com>
2017-04-04 22:35:18 +0100

    Added firebase support Removed unused imports

--- app/src/main/java/com/codingstudiosuk/flirds/FullscreenActivity.java
+++ app/src/main/java/com/codingstudiosuk/flirds/FullscreenActivity.java
@@ -10,10 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
